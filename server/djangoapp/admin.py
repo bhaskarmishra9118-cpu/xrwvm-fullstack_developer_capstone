3eda9890bd28751c53b6c99fc1d9cf3d26039f1a
bhaskarmishra9118-cpu <bhaskar.mishra9118@gmail.com>
2025-12-20 21:44:22 +0530

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,6 +1,8 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
+admin.site.register(CarMake)
+admin.site.register(CarModel)
 
 # Register your models here.
 
@@ -11,3 +13,4 @@
 # CarMakeAdmin class with CarModelInline
 
 # Register models here
+
