fa57b69ea798a356ac84167659f689b4d37a958b
bhaskarmishra9118-cpu <bhaskar.mishra9118@gmail.com>
2026-01-13 20:09:45 +0530

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -12,10 +12,11 @@ urlpatterns = [
      path(route='login', view=views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.registration, name='register'),
-
+path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     path('get_cars', views.get_cars, name='getcars'),
         # path for dealer reviews view
 
     # path for add a review view
-
+    path(route='add_review', view=views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
