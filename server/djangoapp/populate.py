fe034800107e6f20dda65d05e874fe17c47aaaca
bhaskarmishra9118-cpu <bhaskar.mishra9118@gmail.com>
2026-01-13 19:27:25 +0530

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,2 +1,17 @@
+from .models import CarMake, CarModel
+
+
 def initiate():
-    print("Populate not implemented. Add data manually")
+    car_make_data = [
+        {"name":"NISSAN", "description":"Great cars. Japanese technology"},
+        {"name":"Mercedes", "description":"Great cars. German technology"},
+        {"name":"Audi", "description":"Great cars. German technology"},
+        {"name":"Kia", "description":"Great cars. Korean technology"},
+        {"name":"Toyota", "description":"Great cars. Japanese technology"},
+    ]
+
+    car_make_instances = []
+    for data in car_make_data:
+            car_make_instances.append(CarMake.objects.create(name=data['name'], description=data['description']))
+
+
