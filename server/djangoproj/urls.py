1a29893cb5706a203a6778c832c92c2fc5d3c19f
bhaskarmishra9118-cpu <bhaskar.mishra9118@gmail.com>
2026-01-14 14:39:43 +0530

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
             path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html")),
+     path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
